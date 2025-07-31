338b557a0f2502168dcab1ac6c8579a429c379a1
ajdench <andrew.dench@icloud.com>
2025-07-31 22:10:19 +0100

    fix: Set correct base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/3d-model-viewer/',
   build: {
     chunkSizeWarningLimit: 1000, // Set warning limit to 1000 kB
   },
