3f3505e7bd11e2f1ac6e71c5e47155ec7befea82
ajdench <andrew.dench@icloud.com>
2025-08-14 00:27:11 +0100

    fix: Update vite.config.js base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : './';
+  const base = command === 'serve' ? '/' : '/3d-model-viewer/';
   return {
     base,
   };
